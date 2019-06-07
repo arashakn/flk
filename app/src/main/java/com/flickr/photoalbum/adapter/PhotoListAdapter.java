5129df083b805e0dd033ee462e61d2846ca755ef
Arash Alikhani <arashalikhani@MacBook-Pro.local>
2019-06-07 12:59:10 -0700

    update selection

--- app/src/main/java/com/flickr/photoalbum/adapter/PhotoListAdapter.java
+++ app/src/main/java/com/flickr/photoalbum/adapter/PhotoListAdapter.java
@@ -2,13 +2,20 @@ package com.flickr.photoalbum.adapter;
 
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.lifecycle.LifecycleOwner;
+import androidx.recyclerview.selection.ItemDetailsLookup;
+import androidx.recyclerview.selection.ItemKeyProvider;
+import androidx.recyclerview.selection.SelectionTracker;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
 import com.flickr.photoalbum.PhotoAlbumViewModel;
 import com.flickr.photoalbum.R;
 import com.flickr.photoalbum.service.model.Image;
@@ -43,40 +50,7 @@ public class PhotoListAdapter extends RecyclerView.Adapter<PhotoListAdapter.Phot
     @Override
     public void onBindViewHolder(@NonNull PhotoItemViewHolder holder, int position) {
         Image image = data.get(position);
-        String imageURL ="";
-        if(image.getImageUrls().thumb!=null && !image.getImageUrls().thumb.isEmpty()) {
-            imageURL = image.getImageUrls().thumb;
-            Log.d("Adapter + small", imageURL);
-
-        }
-        else{
-            imageURL = image.getImageUrls().large;
-            Log.d("Adapter + large", imageURL);
-
-        }
-
-
-
-        Picasso.with(holder.iv_thumb.getContext().getApplicationContext()).load(imageURL)
-                .fit()
-                .centerCrop()
-                .error(holder.iv_thumb.getContext().getResources().getDrawable(R.drawable.ic_image))
-                .into(holder.iv_thumb, new Callback() {
-                            @Override
-                            public void onSuccess() {
-                                Log.d("suc", "suc: ");
-
-                            }
-
-                            @Override
-                            public void onError() {
-                                Log.d("erorr", "onError: ");
-                            }
-                        }
-
-                )
-        ;
-
+        holder.bind(position,image);
     }
 
     @Override
@@ -90,8 +64,11 @@ public class PhotoListAdapter extends RecyclerView.Adapter<PhotoListAdapter.Phot
         ImageView iv_thumb;
         onPhotoItemClickListener onPhotoItemClickListener;
 
+        private Details details;
+
         public PhotoItemViewHolder(@NonNull View itemView, onPhotoItemClickListener onPhotoItemClickListener) {
             super(itemView);
+            details = new Details();
             tv_image_link = itemView.findViewById(R.id.tv_image_link);
             iv_thumb = itemView.findViewById(R.id.iv_thumb);
             this.onPhotoItemClickListener = onPhotoItemClickListener;
@@ -102,12 +79,115 @@ public class PhotoListAdapter extends RecyclerView.Adapter<PhotoListAdapter.Phot
         public void onClick(View v) {
             onPhotoItemClickListener.onPhotoClick(getAdapterPosition());
         }
+
+        Details getItemDetails() {
+            return details;
+        }
+
+        public void bind(int position, Image image){
+            String imageURL ="";
+            if(image.getImageUrls().thumb!=null && !image.getImageUrls().thumb.isEmpty()) {
+                imageURL = image.getImageUrls().thumb;
+            }
+            else{
+                imageURL = image.getImageUrls().large;
+            }
+
+            Picasso.with(iv_thumb.getContext().getApplicationContext()).load(imageURL)
+                    .fit()
+                    .centerCrop()
+                    .error(iv_thumb.getContext().getResources().getDrawable(R.drawable.ic_image))
+                    .into(iv_thumb);
+        }
+
     }
 
     public interface onPhotoItemClickListener{
         public void onPhotoClick(int position);
     }
 
+    static class Details extends ItemDetailsLookup.ItemDetails<Long> {
+
+        long position;
+
+        Details() {
+        }
+
+        @Override
+        public int getPosition() {
+            return (int) position;
+        }
+
+        @Nullable
+        @Override
+        public Long getSelectionKey() {
+            return position;
+        }
+
+        @Override
+        public boolean inSelectionHotspot(@NonNull MotionEvent e) {
+            return true;
+        }
+    }
+
+    static class KeyProvider extends ItemKeyProvider<Long> {
+
+        KeyProvider(RecyclerView.Adapter adapter) {
+            super(ItemKeyProvider.SCOPE_MAPPED);
+        }
+
+        @Nullable
+        @Override
+        public Long getKey(int position) {
+            return (long) position;
+        }
+
+        @Override
+        public int getPosition(@NonNull Long key) {
+            long value = key;
+            return (int) value;
+        }
+    }
+
+    static class DetailsLookup extends ItemDetailsLookup<Long> {
+
+        private RecyclerView recyclerView;
+
+        DetailsLookup(RecyclerView recyclerView) {
+            this.recyclerView = recyclerView;
+        }
+
+        @Nullable
+        @Override
+        public ItemDetails<Long> getItemDetails(@NonNull MotionEvent e) {
+            View view = recyclerView.findChildViewUnder(e.getX(), e.getY());
+            if (view != null) {
+                RecyclerView.ViewHolder viewHolder = recyclerView.getChildViewHolder(view);
+                if (viewHolder instanceof PhotoItemViewHolder) {
+                    return ((PhotoItemViewHolder) viewHolder).getItemDetails();
+                }
+            }
+            return null;
+        }
+    }
+
+    static class Predicate extends SelectionTracker.SelectionPredicate<Long> {
+
+        @Override
+        public boolean canSetStateForKey(@NonNull Long key, boolean nextState) {
+            return true;
+        }
+
+        @Override
+        public boolean canSetStateAtPosition(int position, boolean nextState) {
+            return true;
+        }
+
+        @Override
+        public boolean canSelectMultiple() {
+            return true;
+        }
+    }
 
 
 }
